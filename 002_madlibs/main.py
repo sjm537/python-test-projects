51e8a76565c50e479cfe36e882826f79f7fc170b
sjm537 <sjmiller592@gmail.com>
2022-03-04 11:50:22 -0700

    Update main.py

--- 002_madlibs/main.py
+++ 002_madlibs/main.py
@@ -7,14 +7,48 @@
 #print("subscribe to {}".format(youtuber))
 #print(f"subscribe to {youtuber}")
 
-adj = input("Adjective: ")
-verb1 = input("Verb: ")
-verb2 = input("Verb: ")
-famous_person = input("Famous Person: ")
+playMadlib = input("Would you like to play? Y or N\n")
 
-madlib = f"Computer programming is so {adj}! \
-It makes me so excited all the time because \
-I love to {verb1}. Stay hydrated and {verb2} \
-like you are {famous_person}!"
+while playMadlib == "Y":
+    n = input("Select Madlib 1 or 2:\n")
+    if n == "1":
+        #Madlib 1
+        adj = input("Adjective: ")
+        verb1 = input("Verb: ")
+        verb2 = input("Verb: ")
+        famous_person = input("Famous Person: ")
 
-print(madlib)
+        madlib1 = f"Computer programming is so {adj}!\n\
+        It makes me so excited all the time because\n\
+        I love to {verb1}. Stay hydrated and {verb2}\n\
+        like you are {famous_person}!"
+
+        print(madlib1)
+
+    elif n == "2":
+        #Madlib 2
+        today = input("Today's Date: ")
+        verb1 = input("Verb: ")
+        animal = input("Animal: ")
+        favFood = input("Favorite Food: ")
+        verb2 = input("Verb: ")
+
+        madlib2 = f"To Do List for {today}\n\
+            1. {verb1} the {animal}\n\
+            2. Cook {favFood} for dinner\n\
+            3. Program app that {verb2}s for me"
+        print(madlib2)
+
+    playMadlib = input("Would you like to play again? Y or N\n")
+
+if playMadlib == "N":
+    print ("Goodbye!")
+
+else:
+    print("That is not a valid selection.")
+
+
+
+
+
+
